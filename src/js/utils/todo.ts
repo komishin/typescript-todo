a4615290171ec6bdbf87903ceff011bdb87df932
古宮信二 <>
2026-02-02 19:11:52 +0900

    行が積み上がらず一回消して、新たにデータを取得するように実装

--- src/js/utils/todo.ts
+++ src/js/utils/todo.ts
@@ -2,12 +2,12 @@
  * todoの型定義
  */
 
-import { createElement, getElementById, getInputElementById } from "./dom";
+import { createElement, getElementById, getInputElementById } from './dom'
 
 export type Todo = {
-    name:string;
-    person:string;
-    deadline:string;
+  name: string
+  person: string
+  deadline: string
 }
 
 /**
@@ -15,26 +15,39 @@ export type Todo = {
  * @returns Todo
  */
 
-export const getNewTodo = (): Todo => ({//returnを使わないで書いている方法（）が決め手
-    name: getInputElementById("new-todo-name").value,
-    person: getInputElementById("new-person").value,
-    deadline: getInputElementById("new-deadline").value
+export const getNewTodo = (): Todo => ({
+  //returnを使わないで書いている方法（）が決め手
+  name: getInputElementById('new-todo-name').value,
+  person: getInputElementById('new-person').value,
+  deadline: getInputElementById('new-deadline').value,
 })
 
 /**
  * DOMにTODO一愛rンを表示する
  */
 
-export const appendTodoList = (todoList:Todo[]) => {
-    todoList.forEach((todo) => {
-        const nameTd = createElement("td", todo.name)
-        const personTd = createElement("td", todo.person)
-        const deadlineTd = createElement("td", todo.deadline)
-        const tr = createElement("tr")
-        tr.appendChild(nameTd)
-        tr.appendChild(personTd)
-        tr.appendChild(deadlineTd)
-        const tBody = getElementById("todo-list")
-        tBody.appendChild(tr)
-    });
+export const appendTodoList = (todoList: Todo[]) => {
+  removeTodoListElement()
+  todoList.forEach((todo) => {
+    const nameTd = createElement('td', todo.name)
+    const personTd = createElement('td', todo.person)
+    const deadlineTd = createElement('td', todo.deadline)
+    const tr = createElement('tr')
+    tr.appendChild(nameTd)
+    tr.appendChild(personTd)
+    tr.appendChild(deadlineTd)
+    const tBody = getElementById('todo-list')
+    tBody.appendChild(tr)
+  })
+}
+
+/**
+ * DOM一覧をすべて削除する
+ */
+const removeTodoListElement = () => {
+  const tBody = getElementById('todo-list')
+  while (tBody.firstChild) {
+    //tBodyがなくなるまで
+    tBody.firstChild.remove()
+  }
 }
