aa7c9b5e49f2a1f92549042dcc82dced37cf8df2
古宮信二 <>
2026-02-02 18:54:23 +0900

    TODOリストを表示できるように実装

---
+++ src/js/utils/todo.ts
@@ -0,0 +1,40 @@
+/**
+ * todoの型定義
+ */
+
+import { createElement, getElementById, getInputElementById } from "./dom";
+
+export type Todo = {
+    name:string;
+    person:string;
+    deadline:string;
+}
+
+/**
+ * DOMのinput要素から新しいTODOの値を取得する
+ * @returns Todo
+ */
+
+export const getNewTodo = (): Todo => ({//returnを使わないで書いている方法（）が決め手
+    name: getInputElementById("new-todo-name").value,
+    person: getInputElementById("new-person").value,
+    deadline: getInputElementById("new-deadline").value
+})
+
+/**
+ * DOMにTODO一愛rンを表示する
+ */
+
+export const appendTodoList = (todoList:Todo[]) => {
+    todoList.forEach((todo) => {
+        const nameTd = createElement("td", todo.name)
+        const personTd = createElement("td", todo.person)
+        const deadlineTd = createElement("td", todo.deadline)
+        const tr = createElement("tr")
+        tr.appendChild(nameTd)
+        tr.appendChild(personTd)
+        tr.appendChild(deadlineTd)
+        const tBody = getElementById("todo-list")
+        tBody.appendChild(tr)
+    });
+}
